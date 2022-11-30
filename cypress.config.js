3556fcabf37c37fe958d41be446c777e4a1cefc8
Robert Kosak <robert.kosak@chimeraprime.com>
2022-11-30 19:08:27 +0100

    Fix: ( cypress  workflow )

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "92kkfa",
   defaultCommandTimeout: 2000,
+  screenshotOnRunFailure: true,
+  videoUploadOnPasses: false,
+  nodeVersion: 'bundled',
+  videoCompression: 10,
   viewportWidth: 1920,
   viewportHeight: 1080,
   env: {
--- cypress/e2e/visit-local-host.spec.cy.js
+++ cypress/e2e/visit-local-host.spec.cy.js
@@ -1,5 +1,7 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000')
+
+    cy.get('a').should('contains.text', 'Learn React');
   })
 })
